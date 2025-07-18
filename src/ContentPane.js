04fd2110a78745609063f8612a2ab36bb5dd7b6d
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-07-18 14:44:57 -0700

    feat: Add content to Projects/Hackathons and new Achievements section

--- src/ContentPane.js
+++ src/ContentPane.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react';
 import "terminal.css";
 import './App.css';
 import './ContentPane.css';
+import { PROJECTS } from './data/projects.js';
+import { HACKATHONS } from './data/hackathons.js';
 
-
-function ProjectItem(url, name) {
-  console.log(url);
+function ProjectItem(props) {
   return (
-    <li>
-      <a href={url} target='_blank' rel='noreferrer'>{name}</a>
-    </li>
+    <a href={props.repo} target='_blank' rel='noreferrer'>
+      <li className='project-item terminal-card'>
+        <header>{props.name}</header>
+        <div>
+          <p>{props.description}</p>
+          <sub className='date'>Created {props.creation}</sub>
+        </div>
+      </li>
+    </a>
+  );
+}
+
+function HackathonItem(props) {
+  return (
+    <div className='hackathon-item terminal-card'>
+      <header><a href={props.site} target='_blank' rel='noreferrer'>{props.name}</a></header>
+      <div>
+        <p><strong>Date:</strong> {props.date}</p>
+        <p><strong>Location:</strong> {props.location}</p>
+        <p><strong>Project made:</strong> <a href={props.project.repo} target='_blank' rel='noreferrer'>{props.project.name}</a></p>
+        <p><strong>Project description:</strong> {props.project.description}</p>
+      </div>
+    </div>
   );
 }
 
@@ -18,8 +38,11 @@ function Projects() {
     <>
       <h2 className='heading'>Projects</h2>
       <p>Here are a few of my open-source projects. You can find all of them on <a href="https://github.com/Tinkering-Townsperson">my GitHub profile</a>.</p>
-      <p>Content coming soon</p>
-
+      <ul className='project-list'>
+        {PROJECTS.map((project) => (
+          <ProjectItem name={project.name} repo={project.repo} description={project.description} tags={project.tags} creation={project.creation} />
+        ))}
+      </ul>
     </>
   );
 }
@@ -28,8 +51,12 @@ function Hackathons() {
   return (
     <>
       <h2 className='heading'>Hackathons</h2>
-      <p>Here is a list of all the Hackathons I've participated in.</p>
-      <p>Content coming soon</p>
+      <p>Here is a timeline of all the Hackathons I've participated in.</p>
+      <div className='terminal-timeline'>
+        {HACKATHONS.map((hackathon) => (
+          <HackathonItem name={hackathon.name} site={hackathon.site} date={hackathon.date} location={hackathon.location} project={hackathon.project} />
+        ))}
+      </div>
     </>
   );
 }
@@ -39,6 +66,16 @@ function Stack() {
     <>
       <h2 className='heading'>Stack</h2>
       <p>This is my primary tech stack.</p>
+      <p>Content coming soon</p>
+      </>
+  );
+}
+
+function Achievements() {
+  return (
+    <>
+      <h2 className='heading'>Achievements</h2>
+      <p>These are some things I'm proud of.</p>
       <p>Content coming soon</p>
       </>
   );
@@ -79,8 +116,18 @@ function ContentPane() {
         >
           Stack
         </a>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            setActiveComponent('achievements');
+          }}
+          className={`link-button ${activeComponent === 'achievements' ? 'active' : ''}`}
+        >
+          Achievements
+        </a>
       </div>
-      <div className="content-pane">{activeComponent === 'projects' ? <Projects /> : activeComponent === 'hackathons' ? <Hackathons /> : activeComponent === 'stack' ? <Stack /> : 7}</div>
+      <div className="content-pane">{activeComponent === 'projects' ? <Projects /> : activeComponent === 'hackathons' ? <Hackathons /> : activeComponent === 'stack' ? <Stack /> : activeComponent === 'achievements' ? <Achievements /> : 7}</div>
     </div>
   );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+export const PROJECTS = [
+	{
+		name: "payamaroufi.xyz",
+		repo: "https://github.com/Tinkering-Townsperson/Tinkering-Townsperson.github.io",
+		description: "My personal portfolio website. Made during Hack Club's High Seas program.",
+		creation: "January 2024"
+	},
+	{
+		name: "SpaceGoose",
+		repo: "https://github.com/FraserHackClub/SpaceGoose",
+		description: "A 2D platformer made for Hack Club's Juice Game Jam in April 2025.",
+		creation: "February 2025"
+	},
+	{
+		name: "fraser.hackclub.com",
+		repo: "https://github.com/FraserHackClub/FraserHackClub.github.io",
+		description: "The website for the student-run coding club Fraser Hack Club, based in Vancouver, BC.",
+		creation: "December 2024"
+	},
+	{
+		name: "HackGrid",
+		repo: "https://github.com/Tinkering-Townsperson/HackGrid",
+		description: "The HackGrid is a custom keyboard that uses the MIDI protocol, one of my submissions to Hack Club's Highway in the Summer of 2025.",
+		creation: "June 2025"
+	},
+	{
+		name: "Nerf-Light",
+		repo: "https://github.com/Tinkering-Townsperson/Nerf-Light",
+		description: "A single-player version of red light, green light, made in Hack Club's Undercity Hackathon in July 2025.",
+		creation: "July 2025"
+	},
+	{
+		name: "Chunky",
+		repo: "https://github.com/TinkerTown-SEMI/Chunky",
+		description: "Chunky is a desktop assistant that breaks down assignments into manageable tasks, helping students stay focused and productive.",
+		creation: "September 2024"
+	}
+]
