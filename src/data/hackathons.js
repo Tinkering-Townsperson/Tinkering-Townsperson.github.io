5463244657785b58853b8487e02ce7037ef10799
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-09-28 18:49:13 -0700

    style: Whitespace changes

--- src/data/hackathons.js
+++ src/data/hackathons.js
@@ -1,35 +1,35 @@
 export const HACKATHONS = [
-	{
-		name: "Undercity",
-		site: "https://highway.hackclub.com/getting-started/undercity",
-		date: "July 2025",
-		location: "San Francisco, United States",
-		project: {
-			name: "Nerf-Light",
-			description: "A single-player version of red light, green light, made in Hack Club's Undercity Hackathon in July 2025.",
-			repo: "https://github.com/Tinkering-Townsperson/Nerf-Light"
-		}
-	},
-	{
-		name: "Juice",
-		site: "https://github.com/hackclub/juice/blob/main/README.md",
-		date: "April 2025",
-		location: "Shanghai, China",
-		project: {
-			name: "SpaceGoose",
-			description: "A 2D platformer made for Hack Club's Juice Game Jam in April 2025.",
-			repo: "https://github.com/FraserHackClub/SpaceGoose"
-		}
-	},
-	{
-		name: "YVRHacks 2024",
-		site: "https://yvrhacks.ca/",
-		date: "September 2024",
-		location: "Vancouver, Canada",
-		project: {
-			name: "Chunky",
-			description: "Chunky is a desktop assistant that breaks down assignments into manageable tasks, helping students stay focused and productive.",
-			repo: "https://github.com/TinkerTown-SEMI/Chunky"
-		}
-	}
+  {
+    name: "Undercity",
+    site: "https://undercity.hackclub.com/",
+    date: "July 2025",
+    location: "San Francisco, United States",
+    project: {
+      name: "Nerf-Light",
+      description: "A single-player version of red light, green light, made in Hack Club's Undercity Hackathon in July 2025.",
+      repo: "https://github.com/Tinkering-Townsperson/Nerf-Light",
+    }
+  },
+  {
+    name: "Juice",
+    site: "https://github.com/hackclub/juice/blob/main/README.md",
+    date: "April 2025",
+    location: "Shanghai, China",
+    project: {
+      name: "SpaceGoose",
+      description: "A 2D platformer made for Hack Club's Juice Game Jam in April 2025.",
+      repo: "https://github.com/FraserHackClub/SpaceGoose",
+    }
+  },
+  {
+    name: "YVRHacks 2024",
+    site: "https://yvrhacks.ca/",
+    date: "September 2024",
+    location: "Vancouver, Canada",
+    project: {
+      name: "Chunky",
+      description: "Chunky is a desktop assistant that breaks down assignments into manageable tasks, helping students stay focused and productive.",
+      repo: "https://github.com/TinkerTown-SEMI/Chunky",
+    }
+  }
 ]
